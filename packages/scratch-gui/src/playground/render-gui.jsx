25604cf24fcb82baf52d69bb77495cd6d0cf89a5
MD <michael.j.davola@gmail.com>
2026-04-05 07:30:59 -0400

    feat: update logo click to link to upshiftlearning.com

--- packages/scratch-gui/src/playground/render-gui.jsx
+++ packages/scratch-gui/src/playground/render-gui.jsx
@@ -9,7 +9,7 @@ import log from '../lib/log.js';
 import {PLATFORM} from '../lib/platform.js';
 
 const onClickLogo = () => {
-    window.location = 'https://scratch.mit.edu';
+    window.location = 'https://www.upshiftlearning.com';
 };
 
 const handleTelemetryModalCancel = () => {
